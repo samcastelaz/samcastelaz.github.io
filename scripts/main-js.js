d95803c968a5484005ff6565736ade8ca27aa6a8
samcastelaz <sam.castelaz@gmail.com>
2017-10-05 15:20:28 -0400

    Added title click, cleared up text generation

--- scripts/main-js.js
+++ scripts/main-js.js
@@ -1,14 +1,38 @@
-var quotes = [
-	'Eat an orange and you can get protection from scurvy.<br/>Eat an iPhone and you can get internal bleeding.',
-	'Your orange gets hacked, you get a knife in your orange.<br/>Your iPhone gets hacked, you get your face stolen',
-	'Everyone remembers what an orange is.<br/>No one remembers the iHome. I mean HomePod. I mean AppleHome. I mean HomeBud.',
-	'Walk around with oranges in your ears and everyone thinks you\'re crazy.<br/>Work around with AirPods and everything thinks you\'re crazy and stuipd',
-	'An orange a day keeps scurvy away.<br/>An iPhone a day keeps finical security away.'
+var first = [
+	'Eat an orange and you can get protection from scurvy.',
+	'Your orange gets hacked, you get a knife in your orange.',
+	'Everyone remembers what an orange is.',
+	'Walk around with oranges in your ears and everyone thinks you\'re crazy.',
+	'An orange a day keeps scurvy away.'
+]
+
+var second = [
+	'Eat an iPhone and you can get internal bleeding.',
+	'Your iPhone gets hacked, you get your face stolen.',
+	'No one remembers the iHome. I mean HomePod. I mean AppleHome. I meanAirBud. I mean HomeBud.',
+	'Work around with AirPods and everything thinks you\'re crazy and stuipd',
+	'An iPhone a day keeps financial security away.'
+]
+
+var titles = [
+	"Fight the power, one peel at a time!",
+	"Orange you glad for this site?",
+	"The site Johnny Appleseed does not want you to know about"
 ]
 
 function generateQuote() {
-	var ranNum = Math.floor(Math.random() * (quotes.length));
-	var text = document.getElementById('quoteText');
-	var quote = quotes[ranNum];
-	text.innerHTML = quote;
+	var ranNum = Math.floor(Math.random() * (first.length));
+	var fact1 = document.getElementById('fact1Text');
+	var fact2 = document.getElementById('fact2Text');
+	var one = first[ranNum];
+	var two = second[ranNum];
+	fact1.innerHTML = one;
+	fact2.innerHTML = two;
+}
+
+function changeTitle() {
+	var ranNum = Math.floor(Math.random() * (titles.length));
+	var titleElement = document.getElementById('title');
+	var title = titles[ranNum];
+	titleElement.innerHTML = title;
 }
